9ccb1d87f2c7d5668407257f12a2978d3453e54d
Pinchuk Igor <pinchukgm@gmail.com>
2021-10-04 10:40:04 +0300

    init

---
+++ app.py
@@ -0,0 +1,19 @@
+import mss
+import keyboard
+import multiprocessing
+import argparse
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--keylogger")
+    parser.add_argument("--screenshots")
+    print(parser.print_help())
+
+
+
